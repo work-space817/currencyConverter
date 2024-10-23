6e8f94a0a096dfce2419a1e3462bd837a00b4416
max-homol <max.homol@lnoks.com>
2024-10-23 20:24:19 +0300

    check

--- src/core/layout/pagesLayout/DefaultLayout.tsx
+++ src/core/layout/pagesLayout/DefaultLayout.tsx
@@ -4,7 +4,7 @@ import DefaultHeader from "./DefaultHeader";
 
 const DefaultLayout = memo(() => {
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full flex">
       <DefaultHeader />
       <div className="flex flex-col py-4 px-6">{<Outlet />}</div>
     </div>
